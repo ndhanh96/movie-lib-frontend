7336e7ec46e5aceede7a7c47a1d799841881c507
ndhanh96 <ndhanh96@gmail.com>
2021-09-29 23:28:15 +0700

    update lazy load

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import loadable from '@loadable/component';
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import Navbar from './app/Navbar';
 import Home from './features/homepage/Home';
-import Login from './features/loginpage/Login'
-import Signup from './features/loginpage/Signup'
-import MoviePage from './features/movie/MoviePage'
-import Footer from './app/Footer'
+import Footer from './app/Footer';
 
 import 'antd/dist/antd.css';
 import './App.scss';
 
+const Login = loadable(() => import('./features/loginpage/Login'));
+const Signup = loadable(() => import('./features/loginpage/Signup'));
+const MoviePage = loadable(() => import('./features/movie/MoviePage'));
+
 function App() {
-  
   return (
     <Router>
       <Navbar />
@@ -31,7 +37,7 @@ function App() {
           <Signup />
         </Route>
         <Route path='/logout'>
-          <Redirect to='/'/>
+          <Redirect to='/' />
         </Route>
         <Route exact path='/'>
           <Home />
